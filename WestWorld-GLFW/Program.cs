1a861b64922df084ba54130d9d620ec4a69e763f
Your Name <you@example.com>
2021-06-05 10:10:37 +0200

    Add project files.

---
+++ WestWorld-GLFW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WestWorld_GLFW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
